96334841a13b0025088131d9e0c3646efb3fecd4
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-11-02 18:38:04 +0100

    Add addTodo function to create a new todo

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -8,6 +8,17 @@ const getTodos = (req, res) => {
   res.json({ status: 'success', data: todos });
 };
 
+const addTodo = (req, res) => {
+  const { title } = req.body;
+  const newTodo = {
+    id: Date.now(),
+    title,
+    completed: false,
+    createdAt: new Date(),
+  };
+  todos.push(newTodo);
+  res.status(201).json({ status: 'success', data: newTodo });
+};
 
 
 
