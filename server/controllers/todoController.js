5a7f924b402fef82d39ee8f05e7275e657f373f1
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-11-02 18:34:48 +0100

    Add getTodos function to retrieve all todos

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,5 +1,19 @@
-const todos = []; // In-memory store for todos
+const todos = []; // Array to store to-do items
+
+// Helper to find a todo by ID
+const findTodoIndexById = (id) => todos.findIndex((todo) => todo.id === id);
+
+// Get all todos
+const getTodos = (req, res) => {
+  res.json({ status: 'success', data: todos });
+};
+
+
+
+
+
+
 
 module.exports = {
-  // Controller functions 
+  getTodos,
 };
