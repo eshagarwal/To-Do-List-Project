40b23f41299bafd45f8560d5dfeaee8d4797ff1f
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-11-03 19:20:40 +0100

    formats index for prod

--- server/api/index.js
+++ server/api/index.js
@@ -1,24 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const todoRoutes = require('../routes/todoRoutes');
+const express = require("express");
+const cors = require("cors");
+const todoRoutes = require("../routes/todoRoutes");
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
 // Connect todo routes
-app.use('/api/todos', todoRoutes);
+app.use("/api/todos", todoRoutes);
 
 // Route to check server
-app.get('/', (req, res) => {
-    res.send('Server is running');
-  });
-
-// Start server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 module.exports = app;
